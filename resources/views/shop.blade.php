026f48ed5818e88ec7df4d8410eef7c800fa0044
JustinIra <junatividad@gbox.adnu.edu.ph>
2023-04-06 20:40:19 +0800

    finished part 2

---
+++ resources/views/shop.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app') <!--layout is folder, app is name-->
+
+@section('content') <!--connected to the yield in the app file-->
+<x-product-list></x-product-list>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [App\Http\Controllers\ProductController::class, 'index'])->name('shop');
