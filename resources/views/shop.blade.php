a5a0bc8752d76aa113f19bad4835b2373b37d000
JustinIra <junatividad@gbox.adnu.edu.ph>
2023-04-07 19:32:47 +0800

    finished part 3

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app') <!--layout is folder, app is name-->
 
 @section('content') <!--connected to the yield in the app file-->
-<x-product-list></x-product-list>
+<x-product-list :products="$products" :admin="$admin">
+    @if($admin)
+    <a href="/shop/add">
+        <button class="btn-dark-green text-white text-uppercase border-0 py-2 px-4">Add Product</button>
+    </a>
+    @endif
+</x-product-list>
 @endsection
